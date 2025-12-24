52698c173918197f3fbf43843dc49c9d5d90aca8
Adam <ajlarson0731@gmail.com>
2025-12-23 23:39:13 -0500

    Implemented getComputerChoice function

---
+++ rock-paper-scissors.js
@@ -0,0 +1,10 @@
+function getComputerChoice() {
+    let num =  Math.floor(3 * Math.random());
+    if (num === 0) {
+        return "rock";
+    } else if (num === 1) {
+        return "paper";
+    } else {
+        return "scissors";
+    }
+}
