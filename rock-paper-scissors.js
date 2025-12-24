22343ff5f1205ff98a1487f07f20bf534abbf619
Adam <ajlarson0731@gmail.com>
2025-12-24 00:24:00 -0500

    Implemented playRound function

--- rock-paper-scissors.js
+++ rock-paper-scissors.js
@@ -11,8 +11,34 @@ function getComputerChoice() {
 
 function getHumanChoice() {
     let choice = prompt("Please enter a choice: ");
-    return choice;
+    return choice.toLowerCase();
 }
 
 let humanScore = 0;
 let computerScore = 0;
+
+function playRound() {
+    let humanChoice = getHumanChoice();
+    let computerChoice = getComputerChoice();
+    if (humanChoice === "rock" && computerChoice === "paper") {
+        computerScore++;
+        console.log("You lose! Paper beats Rock!");
+    } else if (humanChoice === "rock" && computerChoice === "scissors") {
+        humanScore++;
+        console.log("You win! Rock beats Scissors!");
+    } else if (humanChoice === "paper" && computerChoice === "scissors") {
+        computerScore++;
+        console.log("You lose! Scissors beat Paper!");
+    } else if (humanChoice === "paper" && computerChoice === "rock") {
+        humanScore++;
+        console.log("You win! Paper beats rock!");
+    } else if (humanChoice === "scissors" && computerChoice === "rock") {
+        computerScore++;
+        console.log("You lose! Rock beats scissors!");
+    } else if (humanChoice === "scissors" && computerChoice === "paper") {
+        humanScore++;
+        console.log("You win! Scissors beat paper!");
+    } else {
+        console.log("You tied!");
+    }
+}
