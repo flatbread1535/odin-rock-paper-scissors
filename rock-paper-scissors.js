8e35bc7cb062df628484de2e02c76ec000f8f079
Adam <ajlarson0731@gmail.com>
2026-01-09 01:04:28 -0500

    Added small improvements

--- rock-paper-scissors.js
+++ rock-paper-scissors.js
@@ -3,61 +3,68 @@ function getComputerChoice() {
      * Multiplying Math.random() by 3 generates number in domain [0, 3).
      * Then using Math.floor() to floor the number gives a uniform distribution of 0, 1, or 2.
      */
-    let num =  Math.floor(3 * Math.random());
-    if (num === 0) {
-        return "rock";
-    } else if (num === 1) {
-        return "paper";
-    } else {
-        return "scissors";
-    }
-}
-
-function getHumanChoice() {
-    let choice = prompt("Please enter a choice: ");
-    return choice.toLowerCase(); // toLowerCase() makes input case-insensitive
+    const choices = ["rock", "paper", "scissors"];
+    return choices[Math.floor(Math.random() * 3)];
 }
 
 let humanScore = 0;
 let computerScore = 0;
 
-function playRound() {
-    let humanChoice = getHumanChoice();
+const body = document.querySelector("body");
+
+const rockButton = document.createElement("button");
+const paperButton = document.createElement("button");
+const scissorButton = document.createElement("button");
+
+rockButton.textContent = "Rock";
+paperButton.textContent = "Paper";
+scissorButton.textContent = "Scissor";
+
+rockButton.id = "rock";
+paperButton.id = "paper";
+scissorButton.id = "scissors";
+
+body.appendChild(rockButton);
+body.appendChild(paperButton);
+body.appendChild(scissorButton);
+
+const resultDiv = document.createElement("div");
+const scoreDiv = document.createElement("div");
+
+body.appendChild(resultDiv);
+body.appendChild(scoreDiv);
+
+const buttons = document.querySelectorAll("button");
+buttons.forEach(button => {
+    button.addEventListener("click", () => {
+        if (humanScore < 5 && computerScore < 5) {
+            playRound(button.id);
+        }
+    });
+});
+
+function playRound(humanChoice) {
     let computerChoice = getComputerChoice();
-    if (humanChoice === "rock" && computerChoice === "paper") {
-        computerScore++;
-        console.log("You lose! Paper beats Rock!");
-    } else if (humanChoice === "rock" && computerChoice === "scissors") {
+
+    if (humanChoice === computerChoice) {
+        resultDiv.textContent = `Tie. Both chose ${humanChoice}.`;
+    } else if (
+        (humanChoice === "rock" && computerChoice === "scissors") ||
+        (humanChoice === "paper" && computerChoice === "rock") ||
+        (humanChoice === "scissors" && computerChoice === "paper")
+    ) {
         humanScore++;
-        console.log("You win! Rock beats Scissors!");
-    } else if (humanChoice === "paper" && computerChoice === "scissors") {
-        computerScore++;
-        console.log("You lose! Scissors beat Paper!");
-    } else if (humanChoice === "paper" && computerChoice === "rock") {
-        humanScore++;
-        console.log("You win! Paper beats rock!");
-    } else if (humanChoice === "scissors" && computerChoice === "rock") {
-        computerScore++;
-        console.log("You lose! Rock beats scissors!");
-    } else if (humanChoice === "scissors" && computerChoice === "paper") {
-        humanScore++;
-        console.log("You win! Scissors beat paper!");
+        resultDiv.textContent = `You win! ${humanChoice[0].toUpperCase() + humanChoice.slice(1)} beats ${computerChoice}.`;
     } else {
-        console.log("You tied!");
+        computerScore++;
+        resultDiv.textContent = `You lose! ${computerChoice[0].toUpperCase() + computerChoice.slice(1)} beats ${humanChoice}.`;
+    }
+
+    scoreDiv.textContent = `The score is: ${humanScore} - ${computerScore}`;
+
+    if (humanScore === 5) {
+        resultDiv.textContent = "You win!"
+    } else if (computerScore === 5) {
+        resultDiv.textContent = "You lose!"
     }
 }
-
-function playGame() {
-    playRound();
-    console.log("The score is: " + humanScore + " - " + computerScore + ".");
-    playRound();
-    console.log("The score is: " + humanScore + " - " + computerScore + ".");
-    playRound();
-    console.log("The score is: " + humanScore + " - " + computerScore + ".");
-    playRound();
-    console.log("The score is: " + humanScore + " - " + computerScore + ".");
-    playRound();
-    console.log("The score is: " + humanScore + " - " + computerScore + ".");
-}
-
-playGame();
